8ce10c7fb76729eaf21f256eccfe149367c3ed7a
Andrew Malozemoff <ffomezolam@gmail.com>
2022-08-29 16:10:30 -0400

    scraper updates

--- code/scraper.py
+++ code/scraper.py
@@ -20,23 +20,55 @@ class RedditReader:
         self.driver = None
         self.sleep_time = 1
 
+    ### AUTO-CONTROL ('with' statement handling)
     def __enter__(self):
+        " start selenium driver ('with' syntax)"
+        self.open()
+        return self
+
+    def __exit__(self):
+        " close selenium driver ('with' syntax) "
+        self.close()
+        return True # ignore exceptions
+
+    ### DRIVER MANUAL CONTROL
+    def open(self):
+        " start selenium driver "
         self.driver = webdriver.Chrome()
         sleep(self.sleep_time)
         return self
 
+    def close(self):
+        " quit selenium driver "
+        self.driver.quit()
+        return self
+
+    ### DOCUMENT CONTROL
+    def scroll_to(self, to = -1):
+        " scroll to document position "
+        if to < 0: 
+            to = self.get_scroll_height()
+
+        script = f'window.scrollTo(0, {to}, behavior="smooth");'
+        self.driver.execute_script(script)
+        self.sleep()
+        return self
+
+    def get_page_source(self):
+        " Get source code "
+        return self.driver.page_source
+
+    def get_scroll_height(self):
+        " Get page scroll height "
+        return self.driver.execute_script("return document.body.scrollHeight;")
+
+    ### UTILITIES
     def set_sleep_time(self, t = 1):
+        " Set the default sleep time for processing "
         self.sleep_time = t
         return self
 
-    # allow control here
-    def scroll_to(self, to = -1):
-        if to < 0: pass # scroll to bottom
-        else: pass #scroll to position
-        # remember to sleep to allow this to work
-
-    def get_source(self):
-        return self.driver.page_source
-
-    def __exit__(self):
-        self.driver.quit()
+    def sleep(self, t = self.sleep_time):
+        " Sleep for t seconds (wrapper for time.sleep()) "
+        time.sleep(t)
+        return self
