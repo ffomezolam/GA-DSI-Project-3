d5c6f6413579d02b92764b9ee9ede21f8b51dd73
Andrew Malozemoff <ffomezolam@gmail.com>
2022-09-01 00:23:52 -0400

    added searchpostscraper.py

---
+++ code/searchpostscraper.py
@@ -0,0 +1,5 @@
+"""
+Script to get post urls from search results and scrape origin post for each
+result. Necessary due to search results not having body text. Need to load each
+post separately and scrape individually.
+"""
