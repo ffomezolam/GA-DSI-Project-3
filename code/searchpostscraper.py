01324661a663244f20f2abc947354c5bcfc939cc
Andrew Malozemoff <ffomezolam@gmail.com>
2022-09-02 14:05:49 -0400

    searchpostscraper script should be functional now

--- code/searchpostscraper.py
+++ code/searchpostscraper.py
@@ -8,6 +8,8 @@ post separately and scrape individually.
 
 from bs4 import BeautifulSoup
 import os
+import time
+from scraper import RedditReader
 
 # STEP 0: GET SCRAPE FILES
 SCRAPE_DIR = '../scrapes/search/'
@@ -16,10 +18,11 @@ scrape_files = os.listdir(SCRAPE_DIR)
 # STEP 1: GET ALL URLS
 urls = set()
 
+start_time = time.time()
 for file in scrape_files:
     path = SCRAPE_DIR + file
     with open(path, 'r') as f:
-        print(f'opening file: {file}')
+        print(f'opening file: {file}', end='')
         s = BeautifulSoup(f.read(), 'lxml')
 
     links = s.find_all('a', {'data-click-id': 'body'})
@@ -27,6 +30,17 @@ for file in scrape_files:
         url = link.attrs['href']
         urls.add(url)
 
-    print(f'{len(links)} links')
+    print(f'\t> {len(links)} links')
 
-print(f'num urls: {len(urls)}')
+print(f'> num unique urls: {len(urls)}', end=' ')
+print(f'(elapsed time: {time.time() - start_time} seconds)')
+print()
+
+# STEP 2: OPEN SELENIUM AND GET SOURCE FOR EACH POST
+rr = RedditReader()
+
+for url in urls[:3]:
+    rr.url(url)
+    rr.get()
+    rr.write_page_source(prefix='scrape_page', dir='../scrapes/page/')
+    rr.sleep(8)
