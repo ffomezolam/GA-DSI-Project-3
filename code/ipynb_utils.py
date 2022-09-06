2f2a7e661334e19720729bc763a388b679cbedd3
Andrew Malozemoff <ffomezolam@gmail.com>
2022-09-06 11:06:24 -0400

    updated notebook and notebook utility script

--- code/ipynb_utils.py
+++ code/ipynb_utils.py
@@ -4,12 +4,14 @@ Utilities and variables for project notebooks
 
 # IMPORTS
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 import calendar
 
 # VARIABLES
 # pattern for custom tokenizer to allow contractions
 PAT_TOKEN = r"(?u)\b\w\w+\b"
+
+# Date format for my 'time' column
 DATE_FMT = '%Y-%m-%d'
 
 # FUNCTIONS
@@ -33,9 +35,43 @@ def score_report(model, train_tuple, test_tuple):
 
     return string
 
+def metrics_report(model, y, X, labels=['high','low']):
+    """
+    Custom metrics report
+    """
+    preds = model.predict(X)
+    string = ''
+
+    # get classification report
+    string += classification_report(y, preds, target_names=labels)
+    string += '\n'
+
+    # get confusion matrix
+    cm = confusion_matrix(y, preds)
+    tn,fp,fn,tp = cm.flatten()
+    string += f'True Positives: {tp}\n'\
+        + f'True Negatives: {tn}\n'\
+        + f'False Positives: {fp}\n'\
+        + f'False Negatives: {fn}\n'
+
+    return string
+
 def get_year_month_day(dt):
+    """ get year, month, day from datetime object"""
     return (dt.year, dt.month, dt.day)
 
 def get_day_of_week(dt):
     """ Get day of week from datetime object """
     return calendar.weekday(*get_year_month_day(dt))
+
+def df_from_cv(cv, cv_sparse, index):
+    """
+    Make dataframe from sparse matrix returned from CountVectorizer transform
+    """
+    return pd.DataFrame(cv_sparse.A,
+                        columns=cv.get_feature_names_out(),
+                        index=index)
+
+def easy_concat(df1, df2):
+    """ Quick concat df on columns """
+    return pd.concat([df1, df2], axis=1)
